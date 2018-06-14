461d46f1129c6072d6befc07b28c7eea80665648
Federico Arribas <farribas@emmsa.net>
2018-06-14 18:28:34 -0300

    Added auth service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.model);
+    this.authService.login(this.model).subscribe(data => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to log in');
+    });
+  }
+
+  logout() {
+    this.authService.userToken = null;
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+  loggedIn() {
+    return localStorage.getItem('token') != null;
   }
 
 }
