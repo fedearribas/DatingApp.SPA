3d0e998424e6a2c7950351f80af10aa8fddb8ca6
Federico Arribas <farribas@emmsa.net>
2018-06-15 12:34:45 -0300

    Added client login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent,
     NavComponent
+,
+    HomeComponent,
+    RegisterComponent
 ],
   imports: [
     BrowserModule,
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-import { userInfo } from 'os';
 
 @Component({
   selector: 'app-nav',
